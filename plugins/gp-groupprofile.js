722788463f64215d3a3668c8f0ea60bed71f96bb
GDS <112704679+DASTAGHIR@users.noreply.github.com>
2025-01-29 15:42:14 +0500

    Update

---
+++ plugins/gp-groupprofile.js
@@ -0,0 +1,17 @@
+let handler = async (m, { conn, usedPrefix, command }) => {
+  if (!m.isGroup) throw '*Only work in groups!*'
+  if (!m.quoted) throw `*Reply With a image you want keep on group profile*`
+  if (!/image/.test(m.quoted.mimetype)) throw '*Reply with an image!*'
+  
+  let image = await m.quoted.download()
+  await conn.updateProfilePicture(m.chat, image)
+  m.reply('*Group DP successfully changed!*')
+}
+
+handler.help = ['setgdp']
+handler.tags = ['group']
+handler.command = /^(setgdp)$/i
+handler.admin = true
+handler.botAdmin = true
+
+export default handler
