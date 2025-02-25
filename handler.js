3239c45c7892c83a5edcb0bf8365ef9f4911e47f
GDS <112704679+DASTAGHIR@users.noreply.github.com>
2025-02-25 21:11:17 +0500

    Update

--- handler.js
+++ handler.js
@@ -611,7 +611,14 @@ if (statusViewEnabled || bot.statusview) {
 if ((process.env.AutoReaction && process.env.AutoReaction.toLowerCase() === 'true') || (global.db.data.settings[this.user.jid]?.autoreacts)) { if (m.text.match(/(prince|a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z)/gi)) { this.sendMessage(m.chat, { react: { text: (m.sender === '923092668108@s.whatsapp.net') ? "🇵🇰" : pickRandom(["💛", "💛", "🤍", "💗", "♥️", "💛", "💞", "💖", "💓", "❤️", "🧡", "💛", "💚", "💙", "💜", "🖤", "🤍", "💟", "🕊️", "🥀", "🦋", "🐣", "❤‍🩹", "♥️", "🌸", "❣️", "✨", "🎀", "🩷", "🖤", "🤍", "🤎", "💛", "💚", "🩵", "💙", "💜", "💟", "💓", "🩶"]), key: m.key } }); } } function pickRandom(list) { return list[Math.floor(Math.random() * list.length)]; }
 
 
-
+if (m.fromMe && (global.db.data.settings[this.user.jid]?.ownerreacts)) {
+    this.sendMessage(m.chat, { 
+        react: { 
+            text: process.env.owner_react_emojie || "💛", // اگر variable دستیاب نہ ہو تو ایک default emoji
+            key: m.key 
+        } 
+    });
+}
 
 
 
