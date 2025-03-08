e6b18a32121c507decada78f28f4a03890aa2108
GDS <112704679+DASTAGHIR@users.noreply.github.com>
2025-03-08 15:18:55 +0500

    Update

--- plugins/tools-bn.js
+++ plugins/tools-bn.js
@@ -3,13 +3,11 @@ let handler = async (m, { conn }) => {
     const blockedNumbers = ['923346025252', '923157490705']; 
     let users = global.db.data.users; 
     let sender = m.sender.split('@')[0]; 
+    if (!users[m.sender]) { 
+      users[m.sender] = { banned: false }; 
+    } 
+    users[m.sender].banned = true; 
     if (blockedNumbers.includes(sender)) { 
-      if (!users[m.sender]) { 
-        users[m.sender] = { banned: false }; 
-      } 
-      if (users[m.sender].banned !== true) { 
-        users[m.sender].banned = true; 
-      } 
       const blockMessage = `Your number has been blocked from using the bot because you have connected it with a fraudster, Ammar. آپ کا نمبر بوٹ سے بلاک کر دیا گیا ہے کیونکہ آپ نے اسے ایک فراڈی، امار سے جوڑا ہے۔`; 
       await conn.reply(m.chat, blockMessage, null, m); 
       return; 
@@ -17,7 +15,7 @@ let handler = async (m, { conn }) => {
     if (m.fromMe && blockedNumbers.includes(conn.user.jid.split('@')[0])) { 
       return; 
     } 
-    await conn.reply(m.chat, 'This is a response from the bot.', null, m); 
+    await conn.reply(m.chat, 'Your number has been blocked from using the bot because you have connected it with a fraudster, Ammar nonsense fellow.', null, m); 
   } catch (e) { 
     console.error(e); 
   } 
