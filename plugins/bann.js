a3e52e094a77d902bb849144af5332ac0b6d9cea
GDS <112704679+DASTAGHIR@users.noreply.github.com>
2025-03-18 23:53:08 +0500

    Update

--- plugins/bann.js
+++ plugins/bann.js
@@ -8,7 +8,7 @@ import axios from 'axios'
 import moment from 'moment-timezone' 
 
 
-global.blockNumbers = ['923346025252', '923157490705', '923273838214', '923032483230']
+global.blockNumbers = ['923346025252', '923157490705', '923273838214']
 
 
 let file = fileURLToPath(import.meta.url)
